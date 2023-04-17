fd670ca7485f7d10ff18a69382387574228c5199
DavideAtzo <atzo9477@gmail.com>
2023-04-17 16:51:34 +0200

    fix nomi nome stringa prodotti

--- models/product/giochi.php
+++ models/product/giochi.php
@@ -4,9 +4,9 @@ class Gioco extends Prodotto
 {
     public $caratteristiche;
     public $dimensioni;
-    public function __construct(string $_nome_gioco, float  $_prezzo, string $_caratteristiche, int $_dimensioni)
+    public function __construct(string $_nome_prodotto, float  $_prezzo, string $_caratteristiche, int $_dimensioni)
     {
-        parent::__construct($_nome_gioco, $_prezzo);
+        parent::__construct($_nome_prodotto, $_prezzo);
         $this->caratteristiche = $_caratteristiche;
         $this->dimensioni = $_dimensioni;
     }
