4a417d3d1ce79730e29bb65d47125e7bd30a60d6
DavideAtzo <atzo9477@gmail.com>
2023-04-17 18:59:30 +0200

    push prodotti in array

--- index.php
+++ index.php
@@ -11,12 +11,10 @@ $tonno =  new Cibo('Scatoletta di tonno', 3.49, 'gatto', 300, 'tonno, olio');
 $collare =  new Accessorio('Collare in pelle', 4.99, 'Cane', 'Pelle', 20);
 $voliera =  new Accessorio('Voliera per papagalli', 99.99, 'Uccelli', 'Metallo', 120);
 $osso =  new Gioco('Osso giocattolo', 4.99, 'Cane', 'Giocattolo in gomma resistente ai morsi (non tossico', 50);
+$prodotti = [];
+array_push($prodotti, $crocchette, $tonno, $collare ,$voliera, $osso);
+var_dump($prodotti);
 
-var_dump($crocchette);
-var_dump($tonno);
-var_dump($collare);
-var_dump($voliera);
-var_dump($osso);
 
 ?>
 
